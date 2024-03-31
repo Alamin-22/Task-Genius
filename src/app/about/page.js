c74764419ac86c3f33287bea83b68986a0430fbb
Alamin-22 <mdalaminmollick@gmail.com>
2024-04-01 02:20:31 +0600

    crete basic nextjs project

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h2>This page is for about</h2>
+        </div>
+    );
+};
+
+export default AboutPage;
